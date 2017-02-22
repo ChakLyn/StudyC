c60582e720f1a14075a280d01dbea58e04a70f42
ChakLyn <znodemi@gmail.com>
2017-02-22 13:22:31 +0200

    Learn about application domain

---
+++ AppDomain/AppDomain/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fun with the defult AppDomain\n");
+            DispalyDADStuts();
+            Console.ReadLine();
+        }
+
+        private static void DispalyDADStuts()
+        {
+            // Get access to the domain of the program for current thread
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+
+            // Log out different static data about this domain
+            Console.WriteLine($"Name of this domain: {defaultAD.FriendlyName}");
+            Console.WriteLine($"ID of domain in this process: {defaultAD.Id}");
+            Console.WriteLine($"Is this the defualt domain?: {defaultAD.IsDefaultAppDomain()}");
+            Console.WriteLine($"Base dircetory of this domain: {defaultAD.BaseDirectory}");
+        }
+    }
+}
